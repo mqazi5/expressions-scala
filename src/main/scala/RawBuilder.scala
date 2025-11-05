06dff666ca46e05c5c4d51a753bddfe5dfd4761b
mqazi5 <141670970+mqazi5@users.noreply.github.com>
2025-11-05 21:43:59 +0000

    test

--- src/main/scala/RawBuilder.scala
+++ src/main/scala/RawBuilder.scala
@@ -1,15 +1,42 @@
 package edu.luc.cs.laufer.cs371.expressions
 
+import Expr.*
+import Statement.*
+
 object RawBuilder extends ExprParser[Expr, Statement]:
-  override def onExpr = identity[Expr]
-  override def onTerm = identity[Expr]
-  override def onNumber = identity[Expr]
-  override def onPlusFactor = identity[Expr]
-  override def onMinusFactor = identity[Expr]
-  override def onParenExpr = identity[Expr]
-  override def onStatement = identity[Statement]
-  override def onBlock = identity[Statement]
-  override def onAssignment = identity[Statement]
-  override def onIf = identity[Statement]
-  override def onWhile = identity[Statement]
+  override def onExpr(e: Expr ~ Option[String ~ Expr]): Expr = e match
+    case base ~ None => base
+    case base ~ Some(op ~ next) => 
+      if op == "+" then Plus(base, next)
+      else Minus(base, next)
+
+  override def onTerm(t: Expr ~ Option[String ~ Expr]): Expr = t match
+    case factor ~ None => factor
+    case factor ~ Some(op ~ next) =>
+      if op == "*" then Times(factor, next)
+      else if op == "/" then Div(factor, next)
+      else Mod(factor, next)
+
+  override def onNumber(s: String): Expr = Constant(s.toInt)
+  
+  override def onPlusFactor(e: Expr): Expr = e
+  
+  override def onMinusFactor(e: Expr): Expr = UMinus(e)
+  
+  override def onParenExpr(e: Expr): Expr = e
+
+  override def onStatement(s: Statement): Statement = s
+  
+  override def onBlock(statements: List[Statement]): Statement = Block(statements)
+  
+  override def onAssignment(v: String ~ Expr): Statement = v match
+    case variable ~ expr => Assignment(variable, expr)
+  
+  override def onIf(cond: Expr ~ Statement ~ Option[Statement]): Statement = cond match
+    case condition ~ thenBlock ~ elseBlock => If(condition, thenBlock, elseBlock)
+  
+  override def onWhile(w: Expr ~ Statement): Statement = w match
+    case condition ~ body => While(condition, body)
+
+  override def onExpressionStmt(e: Expr): Statement = ExpressionStmt(e)
 end RawBuilder
