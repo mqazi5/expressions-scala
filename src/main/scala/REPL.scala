f47cb3f968cfee6b4ed4f6c9edb8f1d97a61208f
Walter Mikula <146771447+walterMikula@users.noreply.github.com>
2025-11-21 10:08:29 -0600

    Update REPL.scala
    manually updating REPL.scala. issues with pushing on codespace, and pull requests

--- src/main/scala/REPL.scala
+++ src/main/scala/REPL.scala
@@ -7,6 +7,7 @@ import scala.collection.mutable
 import Expr.*
 
 object REPL:
+
   def main(args: Array[String]): Unit =
     println("Welcome to MiniJS Calculator REPL!")
     println("Enter expressions or statements (empty line to exit)")
@@ -15,74 +16,13 @@ object REPL:
     println("  Statement: x = 5; while (x > 0) { x = x - 1; }")
     
     // Environment to store variables
-    val env = mutable.Map[String, Int]()
-    
-    def evaluateInEnv(expr: Expr): Option[Int] = expr match
-      case Constant(c) => Some(c)
-      case Variable(name) => env.get(name)
-      case UMinus(r) => evaluateInEnv(r).map(-_)
-      case Plus(l, r) => 
-        for
-          lv <- evaluateInEnv(l)
-          rv <- evaluateInEnv(r)
-        yield lv + rv
-      case Minus(l, r) =>
-        for
-          lv <- evaluateInEnv(l)
-          rv <- evaluateInEnv(r)
-        yield lv - rv
-      case Times(l, r) =>
-        for
-          lv <- evaluateInEnv(l)
-          rv <- evaluateInEnv(r)
-        yield lv * rv
-      case Div(l, r) =>
-        for
-          lv <- evaluateInEnv(l)
-          rv <- evaluateInEnv(r)
-          if rv != 0
-        yield lv / rv
-      case Mod(l, r) =>
-        for
-          lv <- evaluateInEnv(l)
-          rv <- evaluateInEnv(r)
-          if rv != 0
-        yield lv % rv
-      case Block(exprs) =>
-        var result: Option[Int] = None
-        for expr <- exprs do
-          result = evaluateInEnv(expr)
-        result
-      case ExpressionStmt(expr) =>
-        evaluateInEnv(expr) foreach { result =>
-          println(s"Result: $result")
-        }
-        None
-      case Assignment(variable, expr) =>
-        evaluateInEnv(expr) foreach { value =>
-          env(variable) = value
-          println(s"$variable = $value")
-        }
-        None
-      case If(condition, thenBlock, elseBlock) =>
-        evaluateInEnv(condition) flatMap { value =>
-          if value != 0 then
-            evaluateInEnv(thenBlock)
-          else
-            elseBlock.flatMap(evaluateInEnv)
-        }
-      case While(condition, body) =>
-        var lastResult: Option[Int] = None
-        var continue = true
-        while continue do
-          evaluateInEnv(condition) foreach { value =>
-            if value != 0 then
-              lastResult = evaluateInEnv(body)
-            else
-              continue = false
-          }
-        lastResult
-    
+    // UPDATED: use Value instead of Int
+    val env: Environment = mutable.Map.empty
+
+    //evaulate wiht new environmentr 
+    def evaluateInEnv(expr: Expr) =
+      evaluateWithEnv(expr, env)
+
     var continue = true
     while continue do
       print("\nminijs> ")
@@ -100,9 +40,14 @@ object REPL:
               // Show parsed expression/statement
               println("Parsed:")
               println(expr)
-              // Evaluate
-              evaluateInEnv(expr)
-              // Show current environment
+
+              // Evaluate using the new evaluator
+              val result = evaluateInEnv(expr)
+
+              // Print the result
+              println(s"Result: $result")
+
+              // Show current environment (variables)
               if env.nonEmpty then
                 println("\nCurrent variable values:")
                 env.toList.sortBy(_._1).foreach { (name, value) =>
@@ -116,5 +61,4 @@ object REPL:
           case e: Exception =>
             println(s"Error: ${e.getMessage}")
             println("Please try again")
-
 end REPL
